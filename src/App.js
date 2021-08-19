d4daf82290bab98acde9bb81694b68e1f91845c7
Sambath Selvam <sambath.selvam@e-hps.com>
2021-08-19 16:34:39 +0530

    test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,48 @@ import Crypto from "crypto";
 import EmailEditor from 'react-email-editor'
 
 class App extends Component {
+
+//   useEffect(() => {
+
+//     const signature = Crypto
+//         .createHmac("sha256", "1qTCAXQ9oEJHYrlJkHYQXFsiLBwDIxaTg99zJ29SbijtwUvygpZAbClzuyyCmnMp") // secret key (keep safe!)
+//         .update("655CAA60-7EFE-410C-B13A-0323682F031A")
+//         .digest("hex");
+//     console.log({
+//         user: {
+//             id: "655CAA60-7EFE-410C-B13A-0323682F031A",
+//             signature: signature,
+//             name: 'Sambath Selvam', // optional
+//             email: 's.sambath90@gmail.com' // optional
+//         }
+//     });
+//     console.log("Running HOST:: ", window.location.origin);
+//         setTimeout(() => {
+//             window.unlayer.init({
+//                 id: 'editor-container',
+//                 projectId: 33005,
+//                 displayMode: 'web',
+//                 customJS: [`${window.location.origin}/unlayer/custom.js`],
+//                 tools: {
+//                     'custom#my_tool': {
+//                         data: {
+//                             customHostUrl: window.location.origin
+//                         }
+//                     }
+//                 },
+//                 user: {
+//                     id: "655CAA60-7EFE-410C-B13A-0323682F031A",
+//                     signature: signature,
+//                     name: 'Sambath Selvam', // optional
+//                     email: 's.sambath90@gmail.com' // optional
+//                 }
+//             });
+
+//         }, 1000);
+    
+
+// });
+
   render() {
 
     const signature = Crypto
@@ -14,17 +56,25 @@ class App extends Component {
 
     return (
       <EmailEditor
-        projectId={33005}
+        projectId={167}
         options={{
           customJS: [
-            'https://devapp.trackfiliates.com/unlayer/custom.js',
+            window.location.protocol + '//' + window.location.host + '/custom.js',
           ],
           user: {
             id: "655CAA60-7EFE-410C-B13A-0323682F031A",
             signature: signature,
             name: 'Sambath Selvam', // optional
             email: 's.sambath90@gmail.com' // optional
-        }
+        },
+        tools: {
+              'custom#my_tool': {
+                  data: {
+                      customHostUrl: window.location.origin
+                  }
+              }
+          },
+          displayMode: 'web'
         }}
       />
     );
