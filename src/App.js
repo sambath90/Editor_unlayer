5e385df4e857f829e0079f1a09549b87762e7b8a
Sambath Selvam <sambath.selvam@e-hps.com>
2021-08-19 12:12:52 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Crypto from "crypto";
 import EmailEditor from 'react-email-editor'
 
 class App extends Component {
   render() {
+
+    const signature = Crypto
+    .createHmac("sha256", "1qTCAXQ9oEJHYrlJkHYQXFsiLBwDIxaTg99zJ29SbijtwUvygpZAbClzuyyCmnMp") // secret key (keep safe!)
+    .update("655CAA60-7EFE-410C-B13A-0323682F031A")
+    .digest("hex");
+
     return (
       <EmailEditor
-        projectId={1071}
+        projectId={33005}
         options={{
           customJS: [
             window.location.protocol + '//' + window.location.host + '/custom.js',
-          ]
+          ],
+          user: {
+            id: "655CAA60-7EFE-410C-B13A-0323682F031A",
+            signature: signature,
+            name: 'Sambath Selvam', // optional
+            email: 's.sambath90@gmail.com' // optional
+        }
         }}
       />
     );
